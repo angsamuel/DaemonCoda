882f1949befb9ee6df7f0758d387b0f41b34653e
Samuel Ang <samuel.ang.prog@gmail.com>
2018-06-10 23:35:42 -0500

    Added road building agents.

---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+    public GameObject streetBlock;
+
+    public int level_grid_size = 100;
+    List<StreetCrawler> streetCrawlers;
+    List<Roomie> roomies;
+    GameObject[,] levelGrid;
+	
+    // Use this for initialization
+	void Start () {
+        streetCrawlers = new List<StreetCrawler>();
+        roomies = new List<Roomie>();
+        levelGrid = new GameObject[level_grid_size, level_grid_size];
+
+        Test();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+
+    public void Test()
+    {
+        StreetCrawler sc = new StreetCrawler(this, 5, new Vector2(10, 0), 0, streetBlock);
+        for(int i = 0; i < 9; i++)
+        {
+            sc.Cycle();
+        }
+    }
+
+    public void PlaceBlock(int x, int y, GameObject block)
+    {
+        if(levelGrid[x,y] != null)
+        {
+            Destroy(levelGrid[x, y]);
+        }
+
+        GameObject newBlock = Instantiate(block, transform);
+        newBlock.transform.Translate(new Vector2(x, y));
+        levelGrid[x, y] = newBlock;
+    }
+
+    public bool SpaceIsFree(int x, int y)
+    {
+        return x < level_grid_size && y < level_grid_size && x > -1 && y > -1 && levelGrid[x, y] == null;
+    }
+
+    public bool InBounds(int x, int y)
+    {
+        return x < level_grid_size && y < level_grid_size;
+    }
+
+
+
+}
---
+++ Assets/Scripts/StreetCrawler.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StreetCrawler {
+    GameObject block;
+    LevelGenerator levelGenerator;
+    int width;
+    int prevWidth;
+    int speed = 10;
+    float roomieSpawnChance;
+    float streetCrawlerSpawnChance;
+    int direction;
+    int stoptimer = 50;
+
+    //mutation parameters
+    float chanceToTurn = .8f;
+    float chanceToShrink = .8f;
+    float chanceToSpawnRoomie;
+    float chanceToSpawnStreetCrawler;
+
+    //space between roads 
+    int padding = 0;
+
+    Vector2 gridPos;
+
+    public StreetCrawler(LevelGenerator lg, int sw, Vector2 pos, int orientation, GameObject b)
+    {
+        levelGenerator = lg;
+        width = sw;
+        gridPos = pos;
+        block = b;
+    }
+
+    public void Cycle()
+    {
+        Debug.Log("Taking Cycle");
+        for (int i = 0; i < speed; i++) {
+            
+            Build();
+            Move();
+        }
+        Mutate();
+        Birth();
+        Debug.Log(gridPos);
+    }
+
+
+    void Move()
+    {
+        switch (direction)
+        {
+            case 0:
+                gridPos += new Vector2(0, 1);
+                break;
+            case 1:
+                gridPos += new Vector2(1, 0);
+                break;
+            case 2:
+                gridPos += new Vector2(0, -1);
+                break;
+            case 3:
+                gridPos += new Vector2(-1, 0);
+                break;
+        }
+
+    }
+
+    void MoveBack()
+    {
+        switch (direction)
+        {
+            case 0:
+                gridPos -= new Vector2(0, 1);
+                break;
+            case 1:
+                gridPos -= new Vector2(1, 0);
+                break;
+            case 2:
+                gridPos -= new Vector2(0, -1);
+                break;
+            case 3:
+                gridPos -= new Vector2(-1, 0);
+                break;
+        }
+    }
+
+    void Build()
+    {
+        bool vertical = false;
+        Vector2 orientationCheck = new Vector2(0, 1);
+        if (direction == 0 || direction == 2)
+        {
+            //place vertically
+            vertical = true;
+            orientationCheck = new Vector2(1, 0);
+        }
+
+        //building a road will not destroy existing objects or leave the map
+        bool goodToBuild = true;
+
+        for (int i = 0; i <= width / 2; i++)
+        {
+            if (!levelGenerator.SpaceIsFree((int)gridPos.x + (i * (int)orientationCheck.x) , (int)gridPos.y + (i * (int)orientationCheck.y)) || 
+                !levelGenerator.SpaceIsFree((int)gridPos.x - (i * (int)orientationCheck.x), (int)gridPos.y - (i * (int)orientationCheck.y)))
+            {
+                goodToBuild = false;
+            }
+        }
+
+        if (goodToBuild)
+        {
+            levelGenerator.PlaceBlock((int)gridPos.x, (int)gridPos.y, block);
+            for (int i = 0; i <= width / 2; i++)
+            {
+                levelGenerator.PlaceBlock((int)gridPos.x + (i * (int)orientationCheck.x), (int)gridPos.y + (i * (int)orientationCheck.y), block);
+                levelGenerator.PlaceBlock((int)gridPos.x - (i * (int)orientationCheck.x), (int)gridPos.y - (i * (int)orientationCheck.y), block);
+            }
+        }
+
+    }
+
+    void Mutate()
+    {
+        //shrink
+        float shrinkRoll = Random.Range(0.0f, 1.0f);
+        if (shrinkRoll < chanceToShrink)
+        {
+            Debug.Log("shrinking");
+            RandomShrink();
+        }
+
+        //turn
+        float turnRoll = Random.Range(0.0f, 1.0f);
+        if(turnRoll < chanceToTurn)
+        {
+            Debug.Log("turning");
+            RandomTurn();   
+        }
+    }
+
+   void RandomShrink()
+    {
+        prevWidth = width;
+        width = Random.Range(1, width);
+    }
+
+    void RandomTurn()
+    {
+        for (int i = 0; i < (width / 2) + 1; i++)
+        {
+            MoveBack();
+        }
+
+        if (direction == 0 || direction == 2)
+        {
+            int[] choices = { 1, 3 };
+            direction = choices[Random.Range(0,2)];
+        }
+        else if(direction == 1 || direction == 3)
+        {
+            int[] choices = { 0, 2 };
+            direction = choices[Random.Range(0, 2)];
+        }
+        for (int i = 0; i < prevWidth / 2; i++)
+        {
+            Move();
+        }
+    }
+
+    //produce new agents
+    void Birth()
+    {
+
+    }
+}
